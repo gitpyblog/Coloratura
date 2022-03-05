8b78eb50be223f82c8ec15a72e2abc7377f2fe12
Dawid.S <dawid.baltica@gmail.com>
2022-03-05 01:02:07 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     name=NAME,
     version='0.9.0',
     description='Awesome cprint() function to colored terminal text. Supported full RGB!',
-    # long_description='',
+    long_description=open('README.md').read(),
     keywords='color colour terminal text ansi windows colorama hue',
     url='https://github.com/DawidKos/Coloratura',
     author='pyblog.pl',
